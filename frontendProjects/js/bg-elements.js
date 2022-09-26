dd72a71f63962927796db2e2460184372e99454b
Al1ssaR <krivonosikov_aleksandr@mail.ru>
2022-09-26 23:01:29 +0300

    json не работает на githpages(

--- frontendProjects/js/bg-elements.js
+++
@@ -1,7 +0,0 @@
-const BgElements = ()=>{
-    let elements = document.querySelectorAll('.set-bg')
-    elements.forEach((el) => {
-        el.style.backgroundImage = `url(${el.dataset.setbg})`
-    });
-}
-BgElements()
--- frontendProjects/js/preloader.js
+++
@@ -1,6 +0,0 @@
-const preloader = ()=>{
-    let preloader = document.querySelector('.preloder')
-    setTimeout(() => {
-        preloader.classList.remove('active')
-    }, 500)
-}

14b27a19a8a67c1844bf852b7c71b6257ee43fff
Al1ssaR <krivonosikov_aleksandr@mail.ru>
2024-01-20 18:36:20 +0300

    Скролл полностью доделан
    Доделан скролл, работает ресайз, динамическое изменение ползунка, горизонтальный скролл

--- TestProject/js/text-scroll.js
+++ TestProject/js/text-scroll.js
@@ -1,20 +1,28 @@
 //Вертикальный скролл
 const VerticalScroll = (scrollableContent, scrollableInnerContent, scrollBar) =>{
+  const scrollBarThumb = scrollBar.querySelector('.scroll-bar-thumb');
   let isDraggingV = false;
-  
-
   let isDragging = false;
   let startOffset = 0;
   let startPageY = 0;
   let lastScrollY = 0;
   let ticking = false;
-  let scrollMultiplier = 0.10; // Множитель скорости прокрутки
-  const computedStyle = window.getComputedStyle(scrollableContent);
-  const height = parseInt(computedStyle.height);
+  let scrollMultiplier = 0.40; // Множитель скорости прокрутки
+  let computedStyle = window.getComputedStyle(scrollableContent);
+  let height = parseInt(computedStyle.height);
   scrollBar.style.height = `${height}px`
-  scrollBar.value = 0;
+  //скорость скролла курсором
+  let scrollDragMult ;
+  updateScrollBarV()
+  // Создаем новый экземпляр Resize Observer
+  const resizeObserver = new ResizeObserver(() => {
+    // Обработчик изменений размера внутреннего контейнера
+    updateScrollBarV();
+  });
+  // Передаем целевой элемент для отслеживания изменений размера
+  resizeObserver.observe(scrollableInnerContent);
 
-  scrollBarV.addEventListener('wheel', function(e) {
+  scrollBar.addEventListener('wheel', function(e) {
     e.preventDefault();
     
     const delta = -e.deltaY;
@@ -31,9 +39,9 @@ const VerticalScroll = (scrollableContent, scrollableInnerContent, scrollBar) =>
       ticking = true;
     }
 });
-    scrollBarV.addEventListener('mousedown', function (e) {
+    scrollBar.addEventListener('mousedown', function (e) {
       isDraggingV = true;
-      startPageY = e.pageY - scrollBarV.offsetTop;
+      startPageY = e.pageY - scrollBar.offsetTop;
       startOffset = scrollableInnerContent.offsetTop;
       e.preventDefault();
     });
@@ -41,7 +49,7 @@ const VerticalScroll = (scrollableContent, scrollableInnerContent, scrollBar) =>
     document.addEventListener('mousemove', function (e) {
       if (isDraggingV) {
           const deltaY = e.pageY - startPageY;
-          const newScrollTop = startOffset - deltaY*1.7;
+          const newScrollTop = startOffset - deltaY*scrollDragMult;
           if (!ticking) {
             window.requestAnimationFrame(function() {
               scrollableInnerContent.style.top = Math.min(0, Math.max(-scrollableInnerContent.clientHeight + height, newScrollTop)) + 'px';
@@ -60,10 +68,23 @@ const VerticalScroll = (scrollableContent, scrollableInnerContent, scrollBar) =>
     });
 
     function updateScrollBarV() {
-      const maxScroll = scrollableInnerContent.clientHeight - height + 50;
+
+      const contentHeight = scrollableInnerContent.clientHeight;
+      const containerHeight = scrollableContent.clientHeight;
+      if (contentHeight > containerHeight) {
+      scrollBar.style.height = `${height}px`;
+      const ratio = containerHeight / contentHeight;
+      const scrollBarHeight = containerHeight * ratio;
+      scrollBarThumb.style.height = `${scrollBarHeight}px`
+      const maxScroll = scrollableInnerContent.clientHeight - height ;
+      scrollDragMult = 1 * (height/scrollBarHeight);
       const scrollPercentage = Math.abs((((scrollableInnerContent.offsetTop)/ maxScroll)) * 100);
       const thumbPosition = Math.min(100, Math.max(0, scrollPercentage));
-      scrollBarV.querySelector('.scroll-bar-thumb').style.top = `${thumbPosition}%`;
+      scrollBarThumb.style.top = `${1.02*(1-scrollBarHeight/height)*thumbPosition}%`;
+      } else {
+        scrollBar.style.height = '0';
+        scrollBarThumb.style.height = '0';
+      }
     }
 
 
@@ -128,18 +149,98 @@ const VerticalScroll = (scrollableContent, scrollableInnerContent, scrollBar) =>
 }
 
 
-//Горизонтальный скролл
-const HorizontalScroll = (scrollableContent, scrollableInnerContent, scrollBar) =>{
+// Горизонтальный скролл
+const HorizontalScroll = (scrollableContent, scrollableInnerContent, scrollBar) => {
+  const scrollBarThumb = scrollBar.querySelector('.scroll-bar-thumb');
+  let isDraggingH = false;
   let isDragging = false;
   let startOffset = 0;
-  let startPageX = 0; 
-  let lastScrollX = 0; 
+  let startPageX = 0;
+  let lastScrollX = 0;
   let ticking = false;
-  let scrollMultiplier = 0.10; // Множитель скорости прокрутки
+
   const computedStyle = window.getComputedStyle(scrollableContent);
-  const width = parseInt(computedStyle.width); 
-  scrollBar.style.width = `${width - 2}px`; 
-  scrollBar.value = 0;
+  const width = parseInt(computedStyle.width);
+  scrollBar.style.width = `${width}px`;
+  //скорость скролла курсором
+  let scrollDragMult ;
+  updateScrollBarH()
+
+    // Создаем новый экземпляр Resize Observer
+    const resizeObserver = new ResizeObserver(() => {
+      // Обработчик изменений размера внутреннего контейнера
+      updateScrollBarH();
+    });
+
+    // Передаем целевой элемент для отслеживания изменений размера
+    resizeObserver.observe(scrollableInnerContent);
+
+  
+  scrollBar.addEventListener('wheel', function(e) {
+    e.preventDefault();
+    const delta = -e.deltaY;
+
+    lastScrollX = scrollableInnerContent.offsetLeft + delta ;
+
+    if (!ticking) {
+      window.requestAnimationFrame(function() {
+        scrollableInnerContent.style.left = Math.min(0, Math.max(-scrollableInnerContent.clientWidth + width, lastScrollX)) + 'px';
+        updateScrollBarH();
+        ticking = false;
+      });
+
+      ticking = true;
+    }
+  });
+
+  scrollBar.addEventListener('mousedown', function(e) {
+    isDraggingH = true;
+    startPageX = e.pageX - scrollBar.offsetLeft;
+    startOffset = scrollableInnerContent.offsetLeft;
+    e.preventDefault();
+  });
+
+  document.addEventListener('mousemove', function(e) {
+    if (isDraggingH) {
+      const deltaX = e.pageX - startPageX;
+      const newScrollLeft = startOffset - deltaX * scrollDragMult;
+      if (!ticking) {
+        window.requestAnimationFrame(function() {
+          scrollableInnerContent.style.left = Math.min(0, Math.max(-scrollableInnerContent.clientWidth + width, newScrollLeft)) + 'px';
+          updateScrollBarH();
+          ticking = false;
+        });
+
+        ticking = true;
+      }
+      e.preventDefault();
+    }
+  });
+
+  document.addEventListener('mouseup', function() {
+    isDraggingH = false;
+  });
+
+  function updateScrollBarH() {
+    
+    const contentWidth = scrollableInnerContent.clientWidth;
+    const containerWidth = scrollableContent.clientWidth;
+    if (contentWidth > containerWidth) {
+      scrollBar.style.width = `${width}px`;
+      const ratio = containerWidth / contentWidth;
+      const scrollbarWidth = containerWidth * ratio;
+      scrollBarThumb.style.width = `${scrollbarWidth}px`
+      const maxScroll = contentWidth - containerWidth;
+      scrollDragMult = 1 * (width/scrollbarWidth);
+      const scrollPercentage = Math.abs(((scrollableInnerContent.offsetLeft) / maxScroll) * 100);
+      
+      const thumbPosition = Math.min(100, Math.max(0, scrollPercentage));
+      scrollBarThumb.style.left = `${1.02*(1-scrollbarWidth/width)*thumbPosition}%`;
+    } else {
+      scrollBar.style.width = '0';
+      scrollBarThumb.style.width = '0';
+    }
+  }
 
   // Обработчики событий для касаний на мобильных устройствах
   let lastTouchX = 0;
@@ -155,57 +256,30 @@ const HorizontalScroll = (scrollableContent, scrollableInnerContent, scrollBar) 
     lastTouchX = e.changedTouches[0].clientX;
 
     scrollableInnerContent.style.left = Math.min(0, Math.max(-scrollableInnerContent.clientWidth + width, scrollableInnerContent.offsetLeft - delta)) + 'px';
-    updateScrollBarV();
+    updateScrollBarH();
   });
 
   // Ниже 3 обработчика перетаскивания
-  scrollableInnerContent.addEventListener('mousedown', function (e) {
-      isDragging = true;
-      startPageX = e.pageX; 
-      startOffset = scrollableInnerContent.offsetLeft; 
+  scrollableInnerContent.addEventListener('mousedown', function(e) {
+    isDragging = true;
+    startPageX = e.pageX;
+    startOffset = scrollableInnerContent.scrollLeft;
+    e.preventDefault();
+  });
+
+  document.addEventListener('mousemove', function(e) {
+    if (isDragging) {
+      const delta = e.pageX - startPageX;
+      // Устанавливаем левое положение контента с учетом пределов прокрутки
+      scrollableInnerContent.style.left = Math.min(0, Math.max(-scrollableInnerContent.clientWidth + width, startOffset + delta)) + 'px';
+      updateScrollBarH();
       e.preventDefault();
+    }
   });
 
-  document.addEventListener('mousemove', function (e) {
-      if (isDragging) {
-          const delta = e.pageX - startPageX; 
-          scrollableInnerContent.style.left = Math.min(0, Math.max(-scrollableInnerContent.clientWidth + width, startOffset + delta)) + 'px'; 
-          updateScrollBarV();
-          e.preventDefault();
-      }
+  document.addEventListener('mouseup', function() {
+    isDragging = false;
   });
 
-  document.addEventListener('mouseup', function () {
-      isDragging = false;
-  });
-  // Обработчик события прокрутки колесика мыши
-  scrollableInnerContent.addEventListener('wheel', function (e) {
-      e.preventDefault();
+};
 
-      const delta = -e.deltaX; 
-      lastScrollX = scrollableInnerContent.offsetLeft + delta * scrollMultiplier;
-
-      if (!ticking) {
-          window.requestAnimationFrame(function () {
-              scrollableInnerContent.style.left = Math.min(0, Math.max(-scrollableInnerContent.clientWidth + width, lastScrollX)) + 'px'; 
-              updateScrollBarV();
-              ticking = false;
-          });
-
-          ticking = true;
-      }
-  });
-  // Функция обновления скроллбара
-  function updateScrollBarV() {
-      const maxScroll = scrollableInnerContent.clientWidth - scrollableContent.clientWidth; 
-      const scrollPercentage = (scrollableInnerContent.offsetLeft / maxScroll) * 100; 
-      scrollBar.value = Math.abs(scrollPercentage);
-  }
-  // Обработчик события изменения ползунка
-  scrollBar.addEventListener('input', function () {
-      const maxScroll = scrollableInnerContent.clientWidth - scrollableContent.clientWidth; 
-      const scrollPosition = (scrollBar.value / 100) * maxScroll;
-      scrollableInnerContent.style.left = -scrollPosition + 'px';
-  });
-
-}
--- TestProject/js/use-text-scroll.js
+++ TestProject/js/use-text-scroll.js
@@ -3,4 +3,4 @@ const scrollableInnerContent = document.querySelector('.description-text');
 const scrollBarH = document.getElementById('scrollBarH');
 const scrollBarV = document.querySelector('.scroll-bar-v');
 VerticalScroll(scrollableContent,scrollableInnerContent,scrollBarV)
-// HorizontalScroll(scrollableContent,scrollableInnerContent,scrollBarH)
+HorizontalScroll(scrollableContent,scrollableInnerContent,scrollBarH)
